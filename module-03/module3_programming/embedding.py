98f3c91b226b9a5af0d6eb0b8e0731fe67a60d81
Evan Gronberg <evangronberg@Evans-MacBook-Pro.local>
2024-05-26 16:15:59 -0400

    PA3 setup

--- module-03/module3_programming/embedding.py
+++ module-03/module3_programming/embedding.py
@@ -1,15 +1,19 @@
+# External dependencies
 import torch
 
-'''
+"""
 Complete this class by instantiating a parameter called "self.weight", and
 use it to complete the forward() method. You do not need to worry about backpropogation.
-'''
-class CustomEmbedding(torch.nn.Module):
+"""
 
-	def __init__(self, num_embeddings, embedding_dim):
+class CustomEmbedding(torch.nn.Module):
+	"""
+	"""
+	def __init__(self, num_embeddings: int, embedding_dim: int) -> None:
 		super().__init__()
 		# TODO
 
-	def forward(self, x):
+	def forward(self, x: torch.Tensor) -> torch.Tensor:
+		"""
+		"""
 		# TODO
-		
--- module-03/module3_programming/tokenizer.py
+++ module-03/module3_programming/tokenizer.py
@@ -1,6 +1,8 @@
+# Python-native dependencies
 import json
+from typing import List
 
-'''
+"""
 This class should be constructed with trained tokenizer data:
 vocab_file : a string path to a vocab.txt file
 merges_file : a string path to a merges.json file
@@ -13,39 +15,52 @@ A good sanity check is that decode(encode(x)) should return x.
 
 You may assume that only a single sample is passed in at a time (no batching).
 You can add additional methods, classes, etc as you find helpful.
-'''
+"""
 
 class Tokenizer:
-    
-    def __init__(self, vocab_file, merges_file):
+    """
+    Leverages existing vocabulary and merging files to
+    tokenize (encode) and de-tokenize (decode) strings.
+
+    Arguments:
+        vocab_file:  A .txt file listing vocabulary tokens in order.
+        merges_file: A .json file listing tokenizer merges in order.
+    """
+    def __init__(self, vocab_file: str, merges_file: str) -> None:
+        with open(vocab_file, 'r') as vocab_file_stream:
+            self.vocab = [
+                token.replace('\n', '')
+                for token in vocab_file_stream.readlines()
+            ]
+        with open(merges_file, 'r') as merges_file_stream:
+            self.merges = json.load(merges_file_stream)
+
+    def encode(self, string: str) -> List[int]:
+        """
+        Tokenizes a string into a series of integers (i.e., token IDs).
+
+        Arguments:
+            string:         The string to be encoded.
+        Return Values:
+            encoded_string: The string represented as a list of integers.
+        """
         # TODO
 
+    def decode(self, list_of_integers: List[int]) -> str:
+        """
+        De-tokenizes a list of integers into a string.
 
-    def encode(self, string):
-        '''
-        param string : a string to be encoded
-        returns a list of integers (token ids)
-        '''
-
+        Arguments:
+            list_of_integers: A list of token IDs.
+        Return Values:
+            string:           The decoded string.
+        """
         # TODO
 
+if __name__ == '__main__':
 
-    def decode(self, list_of_integers):
-        '''
-        param list_of_integers : a list of token ids
-        returns a string formed by decoding these ids.
-        '''
-
-        # TODO
-
-
-
-if __name__ == "__main__":
-
-    # example of using this class
-
-    tok = Tokenizer("./vocab.txt", "./merges.json")
-    x = tok.encode("Peter piper picked a peck of pickled peppers.")
+    tok = Tokenizer('./vocab.txt', './merges.json')
+    x = tok.encode('Peter piper picked a peck of pickled peppers.')
     print(x)
     x = tok.decode(x)
-    print(x) # should be our original text.
+    print(x)
