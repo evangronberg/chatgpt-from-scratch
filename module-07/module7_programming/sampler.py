5988c99c8dc021e3342a2e67b94ed26d41eb3591
Evan Gronberg <evangronberg@Evans-MacBook-Pro.local>
2024-06-07 16:08:40 -0400

    PA7 top-p and top-k sampling

--- module-07/module7_programming/sampler.py
+++ module-07/module7_programming/sampler.py
@@ -8,7 +8,7 @@ class Sampler:
 	an LLM model, this will sample the next token ID.
 
 	NOTE: If top_k and top_p are both None, sample from
-	      the whole distribution (same as top_p=1.0).
+	      the whole distribution (same as top_p = 1.0).
 
 	Arguments:
 		top_k:             If specified, only the top k logits should be used
@@ -31,20 +31,30 @@ class Sampler:
 	def __init__(
 		self,
 		top_k: int = None,
-		top_p: int = None,
+		top_p: float = None,
 		frequency_penalty: float = 1.0,
 		presence_penalty: float = 1.0
 	) -> None:
 
-		# TODO
-		pass
+		if top_k is not None and top_p is not None:
+			raise ValueError('top_k and top_p may not BOTH be specified!')
+
+		self.top_k = top_k
+		self.top_p = top_p
+		if top_k is None and top_p is None:
+			self.top_p = 1.0
+
+		self.frequency_penalty = frequency_penalty
+		self.presence_penalty = presence_penalty
 
 	def sample_token(
 		self, raw_unsorted_logits: np.ndarray[float],
 		previous_token_ids: np.ndarray[int]
 	) -> int:
 		"""
-		TBA
+		Selects the next token using either top-k or top-p sampling,
+		with frequency and presence penalties applied to previously
+		occuring words.
 
 		Arguments:
 			raw_unsorted_logits: A one dimensional list of logits representing
@@ -59,11 +69,36 @@ class Sampler:
 			sampled_token_id:    A single token ID, sampled according to the
 			                     specified sampling parameters.
 		"""
+		# TODO: Incorporate penalties
 
-		# TODO
+		logits = torch.tensor(raw_unsorted_logits)
+		logits, indices = torch.sort(logits, descending=True)
+
+		if self.top_p is not None:
+			logits = torch.softmax(logits, dim=0)
+			p_sum, top_p_tokens = 0.0, []
+			for logit, index in zip(logits, indices):
+				if p_sum < self.top_p:
+					p_sum += float(logit)
+					top_p_tokens.append(int(index))
+			top_p_distribution = torch.softmax(logits[top_p_tokens], dim=0)
+			top_p_distribution_sample_index = torch.multinomial(
+				top_p_distribution, num_samples=1)
+			sampled_token_id = top_p_tokens[top_p_distribution_sample_index]
+		else:
+			top_k_tokens = indices[:self.top_k]
+			top_k_distribution = torch.softmax(logits[:self.top_k], dim=0)
+			top_k_distribution_sample_index = torch.multinomial(
+				top_k_distribution, num_samples=1)
+			sampled_token_id = top_k_tokens[top_k_distribution_sample_index]
+
+		return sampled_token_id
 
 	# An alternative way to call sample_token(), for convenience
-	def __call__(self, raw_unsorted_logits, previous_token_ids):
+	def __call__(
+		self, raw_unsorted_logits: np.ndarray[float],
+		previous_token_ids: np.ndarray[int]
+	) -> int:
 		return self.sample_token(raw_unsorted_logits, previous_token_ids)
 
 if __name__ == '__main__':
@@ -73,10 +108,10 @@ if __name__ == '__main__':
 	sequence = [1,2,3,4,5]
 
 	for i in range(10):
-		# fake logits for a vocab of size 500
+		# Fake logits for a vocab of size 500
 		logits = np.random.randn(500)
 
-		# get next token in sequence
+		# Get next token in sequence
 		next_token = sampler(logits, sequence)
 		sequence.append(next_token)
 
