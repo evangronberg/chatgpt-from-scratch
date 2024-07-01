1e5fe880b1164ecb7a5898ef71d23659a4dca16e
Evan Gronberg <evangronberg@Evans-MacBook-Pro.local>
2024-07-01 19:30:31 -0400

    PA4 finalized (+ M4 final slides)

--- module-04/module4_programming/mha.py
+++ module-04/module4_programming/mha.py
@@ -50,21 +50,23 @@ class CustomMHA(torch.nn.Module):
 		d_h = int(self.d_model / self.n_heads)
 		batch_size, seq_length = x.shape[0], x.shape[1]
 		# Reshape t to have shape (B, S, 3, D) for the upcoming
-		# split along dim 2 into 3 tensors q, k, and v
+		# split along dim 2 into 3 tensors q, k, and v of shape (B, S, 1, D)
 		t_reshaped = t.reshape(
 			batch_size, seq_length, 3, self.d_model)
 		# Perform the aforementioned split via the `chunk()` method
 		t_split = t_reshaped.chunk(3, dim=2)
 		# For each of the three chunks resulting from the split, squeeze
-		# out dim 2 which has a magnitude of only 1, then reshape the
-		# chunk into the desired size for q, k, and v: (B, H, S, D/H)
-		q, k, v = [vector.squeeze(2).reshape(
-			batch_size, self.n_heads, seq_length, d_h)
-			for vector in t_split
+		# out dim 2 which has a magnitude of only 1 (resulting in
+		# shape (B, S, D)), then reshape the chunk into (B, S, H, D/H)
+		# then transpose it into (B, H, S, D/H)
+		q, k, v = [
+			vector.squeeze(2).reshape(
+				batch_size, seq_length, self.n_heads, d_h
+			).transpose(1, 2) for vector in t_split
 		]
 		# This is the attention equation
 		y_prime = torch.matmul(torch.softmax(
-			torch.matmul(q, k.mT) / math.sqrt(self.d_model),
+			torch.matmul(q, k.mT) / math.sqrt(d_h),
 		dim=-1), v) # Shape: (B, H, S, D/H)
 		y_prime_reshaped = y_prime.transpose(1, 2).reshape(
 			batch_size, seq_length, -1) # Shape: (B, S, D)
@@ -73,25 +75,49 @@ class CustomMHA(torch.nn.Module):
 
 if __name__ == '__main__':
 
-	# Consider instantiating the official MHA with weights from
-	# the custom MHA to make sure it matches, thus the output
-	# from passing x would match
+	import numpy as np
 
-	class OfficialMHA(torch.nn.Module):
-		def __init__(self, d_model, n_heads):
-			super().__init__()
-			self.mha = torch.nn.MultiheadAttention(
-				d_model, n_heads, batch_first=True)
-		def forward(self, x):
-			y, _ = self.mha(x, x, x)
-			return y
+	D = 6
+	H = 2
+	mha = CustomMHA(D,H)
 
-	mha_custom = CustomMHA(128, 8)
-	mha_official = OfficialMHA(128, 8)
+	# Make some fixed weights
+	# This just makes a really long 1-D np array and
+	# then reshapes it into the size we need
+	tensor1 = torch.tensor(
+		np.reshape(np.linspace(-2.0, 1.5, D*D*3), (D*3,D))
+	).to(torch.float32)
+	tensor2 = torch.tensor(
+		np.reshape(np.linspace(-1.0, 2.0, D*D), (D,D))
+	).to(torch.float32)
+	
+	# Copy these into our MHA weights, so we don't need to
+	# worry about random initializations for testing
+	mha.W_qkv.data = tensor1
+	mha.W_o.data = tensor2
 
-	# 32 samples of length 6 each, with d_model at 128
-	x = torch.randn((32, 6, 128))
-	y_custom = mha_custom(x)
-	y_official = mha_official(x)
-	# All of these shapes should be the same
-	print(x.shape, y_custom.shape, y_official.shape)
+	# Make an input tensor
+	B = 2
+	S = 3
+	x = torch.tensor(
+		np.reshape(np.linspace(-1.0, 0.5, B*S*D), (B,S,D))
+	).to(torch.float32)
+
+	# run
+	y1 = mha(x)
+	print(y1.shape)
+	print(y1)
+
+	"""
+	Should print out:
+
+	torch.Size([2, 3, 6])
+	tensor([[[ 17.2176,   5.5439,  -6.1297, -17.8034, -29.4771, -41.1508],
+         [ 17.4543,   5.5927,  -6.2688, -18.1304, -29.9920, -41.8536],
+         [ 17.6900,   5.6398,  -6.4105, -18.4607, -30.5110, -42.5612]],
+
+        [[ -1.3639,  -0.1192,   1.1256,   2.3703,   3.6151,   4.8598],
+         [ -5.5731,  -1.9685,   1.6361,   5.2407,   8.8453,  12.4499],
+         [ -5.6875,  -2.0716,   1.5444,   5.1603,   8.7762,  12.3922]]],
+       grad_fn=<UnsafeViewBackward0>)
+	"""
